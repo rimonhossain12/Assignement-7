dccf4f0d17cd83f777e25d2ae8c81145dfb5ba62
rimonspi12 <rimonmpi@gmail.com>
2021-09-13 22:49:57 +0600

    card color added

--- js/app.js
+++ js/app.js
@@ -18,10 +18,10 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
-      <h5 class="rating">rating: ${product.rating.rate}</h5>
-      <h5 class="rating">ratingCount: ${product.rating.count}</h5>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <h5 class="rating">Rating: ${product.rating.rate}</h5>
+      <h5 class="rating">Total Rating: ${product.rating.count}</h5>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success btn-style">add to cart</button>
+      <button id="details-btn" class="btn btn-danger btn-style">Details</button></div>
 
       `;
     document.getElementById("all-products").appendChild(div);
